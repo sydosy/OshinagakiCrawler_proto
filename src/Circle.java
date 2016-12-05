abb3a06053d8061efb647a79b86895716a46bffe
dossy <sydosy@gmail.com>
2016-12-05 23:06:29 +0900

    とりあえずフォローからサークル取得の実装

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,11 @@
+import twitter4j.ResponseList;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.User;
+
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Circle {
     private String name;
@@ -45,5 +52,33 @@ public class Circle {
 
     public void setAuthor(Author author) {
         this.author = author;
+    }
+
+    //フォローからサークルさんを見つける
+    public static ArrayList<User> getCircles(long[] twitterIDs, Twitter twitter) throws TwitterException {
+        ArrayList<User> targetUsers = new ArrayList<>();
+        int loopMax = twitterIDs.length / 100;
+        int surplusUsers = twitterIDs.length % 100;
+
+        for (int i = 0; i < loopMax; i++) {
+            //lookupUsersの仕様が100人までの情報しか取得できないため100人ずつ処理
+            ResponseList<User> users = twitter.lookupUsers(Arrays.copyOfRange(twitterIDs, i * 100, (i + 1) * 100));
+            for (User user : users) {
+                if (user.getName().matches(".*@.*日目.*")) {
+                    targetUsers.add(user);
+                }
+            }
+        }
+        //100人ずつやったあとの最後の端数を処理
+        //555人のユーザ名を調べる場合500人は上で処理し,55人をこちらで処理する
+        if (surplusUsers != 0) {
+            ResponseList<User> users = twitter.lookupUsers(Arrays.copyOfRange(twitterIDs, loopMax * 100, loopMax * 100 + surplusUsers));
+            for (User user : users) {
+                if (user.getName().matches(".*日目.*")) {
+                    targetUsers.add(user);
+                }
+            }
+        }
+        return targetUsers;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import twitter4j.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Twitter twitter = new TwitterFactory().getInstance();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input screen_name here");
+        String screenName = br.readLine();
+        try {
+            IDs follow = twitter.getFriendsIDs(screenName, -1);
+            System.out.println(follow.getIDs().length);
+
+            User[] circles = Circle.getCircles(follow.getIDs(), twitter).toArray(new User[0]);
+            for (User user: circles
+                 ) {
+                System.out.println(user.getName());
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
     }
 }
